1096cc92b6dc59ce2ee91c660787f96a912ee899
chuchulev-sky <daniel@skyeystudio.com>
2025-08-19 10:46:34 +0300

    Revamp TransactionsScreen layout: Introduced ScrollView for improved navigation, enhanced layout with AntDesign icon, and added a new button for user interaction.

--- app/(tabs)/transactions.tsx
+++ app/(tabs)/transactions.tsx
@@ -1,9 +1,22 @@
-import { Text, View } from 'react-native';
+import { AntDesign } from '@expo/vector-icons';
+import { ScrollView, Text, TouchableOpacity, View } from 'react-native';
 
 export default function TransactionsScreen() {
   return (
-    <View className='flex-1 items-center justify-center bg-stone-950'>
-      <Text className="text-white">Transactions Screen</Text>
+    <ScrollView className='flex-1 bg-stone-950'>
+    <View className='flex items-center justify-start bg-stone-950 p-4 top-32'>
+      <View className='flex-row items-end justify-start w-full gap-2'>
+        <View className='bg-[#CBFD03] rounded-xl p-2'>
+          <AntDesign name="swap" size={40} color="black" />
+        </View>
+        <Text className='text-white text-5xl font-bold'>Transactions</Text>
+      </View>
+      <View className='flex-row items-start justify-start w-full'>
+        <TouchableOpacity className='bg-[#CBFD03] w-24 rounded-md p-2 items-center justify-center'>
+          <Text className="text-black">Hello</Text>
+        </TouchableOpacity>
+      </View>
     </View>
+    </ScrollView>
   );
 }
