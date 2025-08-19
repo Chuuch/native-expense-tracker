e9394d99cef26e012ba7bd72f9d3d8e46ff1a84d
chuchulev-sky <daniel@skyeystudio.com>
2025-08-19 10:46:00 +0300

    Enhance HomeScreen layout: Added ScrollView for better scrolling experience, updated layout structure, and integrated Feather icon for the dashboard section.

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,11 +1,22 @@
-import { Text, TouchableOpacity, View } from 'react-native';
+import { Feather } from '@expo/vector-icons';
+import { ScrollView, Text, TouchableOpacity, View } from 'react-native';
 
 export default function HomeScreen() {
   return (
-    <View className='flex-1 items-center justify-center bg-stone-950'>
-      <TouchableOpacity className='bg-[#CBFD03] w-24 rounded-md p-2 items-center justify-center'>
-        <Text className="text-black">Hello</Text>
-      </TouchableOpacity>
+    <ScrollView className='flex-1 bg-stone-950'>
+    <View className='flex items-center justify-start bg-stone-950 p-4 top-32'>
+      <View className='flex-row items-end justify-start w-full gap-2'>
+        <View className='bg-[#CBFD03] rounded-xl p-2'>
+          <Feather name="bar-chart-2" size={40} color="black" />
+        </View>
+        <Text className='text-white text-5xl font-bold'>Dashboard</Text>
+      </View>
+      <View className='flex-row items-start justify-start w-full'>
+        <TouchableOpacity className='bg-[#CBFD03] w-24 rounded-md p-2 items-center justify-center'>
+          <Text className="text-black">Hello</Text>
+        </TouchableOpacity>
+      </View>
     </View>
+    </ScrollView>
   );
 }
