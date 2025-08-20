b623b1859244c7a26439f946b20065d383bd82b7
chuchulev-sky <daniel@skyeystudio.com>
2025-08-20 16:26:45 +0300

    Refactor AnalyticsScreen layout: Simplified structure by adjusting padding and margins, enhancing readability and visual consistency. Moved ScrollView inside a parent View for better layout management.

--- app/(tabs)/analytics.tsx
+++ app/(tabs)/analytics.tsx
@@ -5,28 +5,27 @@ import me from '../../assets/images/me.png';
 
 export default function AnalyticsScreen() {
   return (
-    <ScrollView 
-      className='flex-1 bg-stone-950'
-      showsVerticalScrollIndicator={false}
-      contentContainerStyle={{ paddingBottom: 140 }}
-    >
-      <View className='flex items-center justify-start bg-stone-950 p-4 top-20 gap-8'>
-        {/* Header */}
-        <View className='flex-row items-end justify-start w-full gap-2'>
-          <View className='flex-row items-center justify-between w-full'>
-            <View className='flex-row items-end justify-center gap-4'>
-
-            
+    <View className='flex-1 bg-stone-950 pt-20'>
+      {/* Header */}
+      <View className='flex-row items-end justify-start w-full gap-2 p-4'>
+        <View className='flex-row items-center justify-between w-full'>
+          <View className='flex-row items-end justify-center gap-4'>
             <View className='bg-[#CBFD03] rounded-full p-2'>
               <Ionicons name="pie-chart-outline" size={30} color="black" />
             </View>
             <Text className='text-white text-4xl font-bold'>Analytics</Text>
-            </View>
-            <View className='flex-row items-center justify-center'>
+          </View>
+          <View className='flex-row items-center justify-center'>
             <Image source={me} alt='Profile' className='w-12 h-12 object-contain self-center rounded-full'/>
-            </View>
           </View>
         </View>
+      </View>
+
+      <ScrollView 
+        className='flex-1 bg-stone-950 px-4'
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={{ paddingBottom: 140, gap: 32 }}
+      >
 
         {/* Time Period Selector */}
         <View className='w-full'>
@@ -232,7 +231,7 @@ export default function AnalyticsScreen() {
             </TouchableOpacity>
           </View>
         </View>
-      </View>
-    </ScrollView>
+      </ScrollView>
+    </View>
   );
 }
