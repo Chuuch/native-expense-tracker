4ffafad5fba94e11d0e3b87b3130dae559f9870c
chuchulev-sky <daniel@skyeystudio.com>
2025-08-20 16:27:38 +0300

    Refactor RateAppScreen layout: Improved structure by moving ScrollView inside a parent View, adjusting padding and spacing for better visual consistency and readability. Enhanced layout management with updated styles.

--- app/(tabs)/settings/rate.tsx
+++ app/(tabs)/settings/rate.tsx
@@ -72,27 +72,28 @@ export default function RateAppScreen() {
   };
 
   return (
-    <ScrollView 
-      className='flex-1 bg-stone-950'
-      showsVerticalScrollIndicator={false}
-      contentContainerStyle={{ paddingBottom: 140 }}
-    >
-      <View className='flex items-center justify-start bg-stone-950 p-4 gap-8 top-20'>
-        {/* Header */}
-        <View className='flex-row items-center justify-between w-full'>
-          <TouchableOpacity 
-            className='bg-stone-800 rounded-xl p-2'
-            onPress={() => router.back()}
-          >
-            <Feather name="arrow-left" size={24} color="#CBFD03" />
-          </TouchableOpacity>
-          <View className='flex-row items-center gap-2'>
-            <Text className='text-white text-4xl font-bold'>Rate App</Text>
-          </View>
-          <TouchableOpacity className='bg-stone-800 rounded-xl p-2'>
-            <Feather name="info" size={24} color="#CBFD03" />
-          </TouchableOpacity>
+    <View className='flex-1 bg-stone-950 pt-20'>
+      {/* Header */}
+      <View className='flex-row items-center justify-between w-full p-4'>
+        <TouchableOpacity 
+          className='bg-stone-800 rounded-xl p-2'
+          onPress={() => router.back()}
+        >
+          <Feather name="arrow-left" size={24} color="#CBFD03" />
+        </TouchableOpacity>
+        <View className='flex-row items-center gap-2'>
+          <Text className='text-white text-4xl font-bold'>Rate App</Text>
         </View>
+        <TouchableOpacity className='bg-stone-800 rounded-xl p-2'>
+          <Feather name="info" size={24} color="#CBFD03" />
+        </TouchableOpacity>
+      </View>
+
+      <ScrollView 
+        className='flex-1 bg-stone-950 px-4'
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={{ paddingBottom: 140, gap: 32 }}
+      >
 
         {/* Welcome Message */}
         <View className='w-full bg-stone-800 rounded-xl p-6'>
@@ -266,7 +267,7 @@ export default function RateAppScreen() {
             </TouchableOpacity>
           </View>
         </View>
-      </View>
-    </ScrollView>
+      </ScrollView>
+    </View>
   );
 }
