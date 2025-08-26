ec0e07f4cd5daf597c01e192d719c669ebdd80ce
chuchulev-sky <daniel@skyeystudio.com>
2025-08-26 15:26:06 +0300

    Refactor RootLayout: Integrate AuthGuard and AuthProvider for enhanced authentication flow, ensuring secure access to the application layout.

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,21 +3,25 @@ import { Stack } from "expo-router";
 import { View } from "react-native";
 import "react-native-reanimated";
 import { SafeAreaProvider } from "react-native-safe-area-context";
+import AuthGuard from "../components/AuthGuard";
+import { AuthProvider } from "../contexts/AuthContext";
 import { ThemeProvider } from "../contexts/ThemeContext";
 import "../global.css";
 
 function RootLayoutContent() {
   return (
-    <View style={{ flex: 1, backgroundColor: '#0c0a09' }}>
-      <SafeAreaProvider>
-        <Stack screenOptions={{ headerShown: false }} >
-          <Stack.Screen name="index" />
-          <Stack.Screen name="(auth)" />
-          <Stack.Screen name="(onboarding)" />
-          <Stack.Screen name="(tabs)" />
-        </Stack>
-      </SafeAreaProvider>
-    </View>
+    <AuthGuard>
+      <View style={{ flex: 1, backgroundColor: '#0c0a09' }}>
+        <SafeAreaProvider>
+          <Stack screenOptions={{ headerShown: false }} >
+            <Stack.Screen name="index" />
+            <Stack.Screen name="(auth)" />
+            <Stack.Screen name="(onboarding)" />
+            <Stack.Screen name="(tabs)" />
+          </Stack>
+        </SafeAreaProvider>
+      </View>
+    </AuthGuard>
   );
 }
 
@@ -27,13 +31,14 @@ export default function RootLayout() {
   });
 
   if (!loaded) {
-    // Async font loading only occurs in development.
     return null;
   }
 
   return (
-    <ThemeProvider>
-      <RootLayoutContent />
-    </ThemeProvider>
+    <AuthProvider>
+      <ThemeProvider>
+        <RootLayoutContent />
+      </ThemeProvider>
+    </AuthProvider>
   );
 }
