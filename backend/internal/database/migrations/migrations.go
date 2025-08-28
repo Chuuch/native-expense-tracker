27c51e61b1ca9c12da78bcfd8d308743e6f47635
chuchulev-sky <daniel@skyeystudio.com>
2025-08-28 14:13:39 +0300

    Add migration function: Implemented RunMigrations function to automate database schema updates for User, Transaction, SavingsGoal, and SavingsContribution models.

---
+++ backend/internal/database/migrations/migrations.go
@@ -0,0 +1,16 @@
+package migrations
+
+import (
+	"money-mate/internal/models"
+
+	"gorm.io/gorm"
+)
+
+func RunMigrations(db *gorm.DB) error {
+	return db.AutoMigrate(
+		&models.User{},
+		&models.Transaction{},
+		&models.SavingsGoal{},
+		&models.SavingsContribution{},
+	)
+}
