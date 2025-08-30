e7530cd58ce165720ca9313977d0d7d54ccb3248
Chuuch <daniel.chuchulev96@gmail.com>
2025-08-29 21:41:25 -0700

    Add hooks index file: Create an index file to export various hooks including `useAnalytics`, `useAuth`, `useSavingsGoal`, `useSavingsGoals`, `useTransaction`, and `useTransactions` for improved modularity and easier access.

---
+++ hooks/index.ts
@@ -0,0 +1,5 @@
+export { useAnalytics } from './useAnalytics';
+export { useAuth } from './useAuth';
+export { useSavingsGoal, useSavingsGoals } from './useSavingsGoals';
+export { useTransaction, useTransactions } from './useTransactions';
+
